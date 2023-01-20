31e802f5cf0ca2b683a371d6d4743c9a5226a6d7
yuji-png <maroro.aws@gmail.com>
2023-01-20 21:12:58 +0900

    add function

---
+++ app.js
@@ -0,0 +1,5 @@
+$("#addTodo").click(function (){
+    const inputTodo = $("input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+    $("input").val("");
+});
